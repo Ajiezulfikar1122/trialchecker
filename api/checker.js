512e372e54fcd413790defa5348ab9e3acb230ee
Ajiezulfikar1122 <142336620+Ajiezulfikar1122@users.noreply.github.com>
2026-05-27 23:10:09 +0700

    Update checker.js

--- api/checker.js
+++ api/checker.js
@@ -24,12 +24,12 @@ module.exports = async (req, res) => {
       ]
     });
 
-    const page = await browser.newPage();
+    const context = await browser.newContext({
+      userAgent:
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+    });
 
-    // USER AGENT
-    await page.setUserAgent(
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
-    );
+    const page = await context.newPage();
 
     // BUKA LOGIN GOOGLE
     await page.goto(
